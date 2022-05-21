bddddb0b9e90026cab81040acc98e63716ecc674
Leonam Sousa <leonamgm18@gmail.com>
2022-05-21 19:06:48 -0300

    Initial Commit - Start Project

---
+++ src/test/java/br/ucsal/company_Joker/CompanyJokerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ucsal.company_Joker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyJokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
